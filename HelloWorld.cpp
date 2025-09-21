0366cbc4b000163449a63946b2e77ee54ef63c26
Sakshi <sakshidk1502@gmail.com>
2025-09-21 18:58:58 +0530

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    // Complete this function
+    void helloWorld() {
+        // cout will output to console
+        cout << "Hello World";
+    }
+};
